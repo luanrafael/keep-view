40f7367ddf02ca319189bf0d0e1369222d0494bd
Lucas Salvador <lucas@superclient.com.br>
2020-01-27 17:19:06 -0300

    adicionando pasta client/

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Notes/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Notes from './components/Notes';
+
+export default {
+  name: 'App',
+
+  components: {
+    Notes,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
