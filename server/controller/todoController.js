b27438a3ae960b7b28963a5619a2943c57048fc8
Lucas Salvador <lucas@superclient.com.br>
2020-01-28 19:06:03 -0300

    adicionando controller

--- server/controller/todoController.js
+++ server/controller/todoController.js
@@ -9,9 +9,35 @@ async function store(req,res)  {
         style
     });
 
-    return res.json(todo);
+    return res.json(todo).status(200)
+}
+
+async function index(req, res) {
+    const todos = await Todo.find()
+    return res.json(todos).status(200)
+}
+
+async function remove(req, res) {
+    const todo = await Todo.find({_id: req.params.id}).remove()
+    res.json(todo).status(200)
+}
+
+async function update(req, res) {
+    const { title, text , style } = req.body
+    var todo = await Todo.findById(req.params.id)
+
+    todo.title = title
+    todo.text = text 
+    todo.style = style
+
+    todo.save()
+
+    res.json(todo).status(200)
 }
 
 module.exports = {
     store,
+    index,
+    remove,
+    update,
 }
