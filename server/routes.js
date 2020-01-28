cc4b604e12437b37117b7e28f3e3eed805772d5c
Lucas Salvador <lucas@superclient.com.br>
2020-01-28 19:06:26 -0300

    adicionando rotas

--- server/routes.js
+++ server/routes.js
@@ -5,5 +5,8 @@ const routes = Router()
 
 
 routes.post('/todo', todoController.store)
+routes.get('/todo', todoController.index)
+routes.delete('/todo/:id', todoController.remove)
+routes.put('/todo/:id', todoController.update)
 
 module.exports = routes
